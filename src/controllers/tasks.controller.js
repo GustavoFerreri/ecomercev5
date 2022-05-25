378db36b80c3bb806428d6c03f5ba1e3b44ce64c
Gustavo Ferreri <ferreri.gustavo@gmail.com>
2022-05-25 18:15:01 -0300

    Se implementa el metodo post

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,9 +1,15 @@
 const db = require('./../services/db');
 
-const list = async () =>
+const list = async (params = {}) =>
     await db(process.env.T_PRODUCT)
+    .where(params)
     .select('id', 'title', 'img', 'description')
 
+const create = (obj) => 
+    db(process.env.T_PRODUCT)
+    .insert(obj)
+
 module.exports = { 
-    list 
+    list,
+    create
 };
--- src/models/product.js
+++ src/models/product.js
@@ -3,8 +3,23 @@ const router = Router();
 const services = require ('./../controllers/tasks.controller')
 
 router.get('/', (req, res) =>
-    services.list()
+    services
+    .list()
     .then(response => res.json(response))
     .catch(e=> res.json({e})));
+
+router.get('/:id', (req, res) =>
+    services
+    .list({id: req.params.id})
+    .then(response => res.json(response))
+    .catch(e=> res.json({e})));
+
+router.post('/', (req, res) =>{
+    const newProduct = ({title, price, img, decription} = req.body);
+    services
+    .create(newProduct)
+    .then(response => res.json(response))
+    .catch(e=> res.json({e}));
+})
 
 module.exports = router;
