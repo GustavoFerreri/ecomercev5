8a4865f56517c46ddec1779f790aed87ad8e990c
Gustavo Ferreri <ferreri.gustavo@gmail.com>
2022-05-25 18:34:25 -0300

    Se implementa update y delete

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -3,13 +3,25 @@ const db = require('./../services/db');
 const list = async (params = {}) =>
     await db(process.env.T_PRODUCT)
     .where(params)
-    .select('id', 'title', 'img', 'description')
+    .select('id', 'title', 'img', 'description');
 
 const create = (obj) => 
     db(process.env.T_PRODUCT)
-    .insert(obj)
+    .insert(obj);
+
+const update = (params, obj) => 
+    db(process.env.T_PRODUCT)
+    .where(params)
+    .update(obj);
+
+const destroy = (params, obj) => 
+    db(process.env.T_PRODUCT)
+    .where(params)
+    .del();
 
 module.exports = { 
     list,
-    create
+    create,
+    update,
+    destroy
 };
--- src/models/product.js
+++ src/models/product.js
@@ -16,10 +16,24 @@ router.get('/:id', (req, res) =>
 
 router.post('/', (req, res) =>{
     const newProduct = ({title, price, img, decription} = req.body);
-    services
+    return services
     .create(newProduct)
     .then(response => res.json(response))
     .catch(e=> res.json({e}));
 })
+
+router.put('/:id', (req, res) =>{
+    const updateProduct = ({title, price, img, decription} = req.body);
+    return services
+    .update(req.params.id, updateProduct)
+    .then(response => res.json(response))
+    .catch(e=> res.json({e}));
+})
+
+router.delete('/:id', (req, res) =>
+    services
+    .destroy({id: req.params.id})
+    .then(response => res.json(response))
+    .catch(e=> res.json({e})));
 
 module.exports = router;
